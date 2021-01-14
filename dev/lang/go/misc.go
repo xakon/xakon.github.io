87d2bf4d087be60b21309003bb0d58e8c15af7fc
Christos Kontas <xakon@yahoo.com>
2021-01-15 00:00:48 +0100

    go: Go Slice Tricks Cheat Sheet

--- dev/lang/go/misc.go
+++ dev/lang/go/misc.go
@@ -1,3 +1,6 @@
 Use `bufio.Scanner` to split a stream into lines.
 
 https://go.libhunt.com/
+
+[Go Slice Tricks Cheat Sheet](https://ueokande.github.io/go-slice-tricks/):
+A visualisation of the official [Go Wiki page](https://ueokande.github.io/go-slice-tricks/).
